8f6f81887f32ea0d51aff53a2c5d6355b9d0f7d3
sanjay007 <iamsanjaym2701@gmail.com>
2024-10-01 21:52:23 +0530

    Encapsulation

---
+++ EncapsulationMain.java
@@ -0,0 +1,14 @@
+package oops.encapsulation;
+
+public class EncapsulationMain {
+	
+	public static void main(String[] args) {
+		Encapsulation d=new Encapsulation();
+		
+		d.setId(1);
+		d.setName("sanjay");
+		
+		System.out.println("id "+d.getId()+" name  "+d.getName() );
+	}
+
+}
